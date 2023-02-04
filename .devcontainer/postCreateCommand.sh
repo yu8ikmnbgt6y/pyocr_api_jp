d8de0c56efcdc6d56d7c04b7fc80de6aedc7f880
yu8ikmnbgt6y <yu8ikmnbgt6y@gmail.com>
2023-02-04 19:02:33 +0000

    add devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,4 @@
+apt update && apt -y upgrade
+sudo apt -y install tesseract-ocr tesseract-ocr-jpn libtesseract-dev libleptonica-dev tesseract-ocr-script-jpan tesseract-ocr-script-jpan-vert 
+
+pip install -U pip && pip install -r requirements.txt
---
+++ test.py
@@ -0,0 +1,75 @@
+from PIL import Image, ImageDraw
+import sys
+
+import pyocr
+import pyocr.builders
+
+tools = pyocr.get_available_tools()
+
+if len(tools) == 0:
+    print("no tools")
+    raise SystemError()
+    exit()
+
+print("available_tools: ", ",".join([x.get_name() for x in tools]))
+tool = tools[0]
+
+
+langs = tool.get_available_languages()
+if len(langs) == 0:
+    print("no langs")
+    raise SystemError()
+    exit()
+
+print("available_langages: ", ",".join(langs))
+# tgt_lang  = langs[0]
+tgt_lang = "jpn"
+
+pyocr_txt_builder = pyocr.builders.TextBuilder()
+pyocr_wb_builder = pyocr.builders.WordBoxBuilder()
+pyocr_lb_builder = pyocr.builders.LineBoxBuilder()
+pyocr_dg_builder = pyocr.builders.DigitBuilder()
+
+
+srcImg = Image.open('48.png')
+dstImg = Image.open('48.png')
+draw = ImageDraw.Draw(dstImg)
+
+
+txt = tool.image_to_string(
+    srcImg,
+    lang=tgt_lang,
+    builder=pyocr_txt_builder
+)
+print(txt)
+
+
+wbs = tool.image_to_string(
+    srcImg,
+    lang=tgt_lang,
+    builder=pyocr_wb_builder
+)
+for wb in wbs:
+    print(wb.content)
+    draw.rectangle(wb.position, outline=(255,0,0), width=2)
+
+lbs = tool.image_to_string(
+    srcImg,
+    lang=tgt_lang,
+    builder=pyocr_lb_builder
+)
+
+for lb in lbs:
+    print(lb.content)
+    draw.rectangle(lb.position, outline=(0,0,255), width=2)
+
+
+digits = tool.image_to_string(
+    srcImg,
+    lang=tgt_lang,
+    builder=pyocr_dg_builder
+)
+print(digits)
+
+
+dstImg.save("dst.png")
